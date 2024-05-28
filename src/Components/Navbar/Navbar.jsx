663d4e9c178c09cfa92441d5c143c81f8d041f89
EsadaDurmic <esada.durmic.23@size.ba>
2024-05-28 14:34:25 +0200

    navbar update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,70 +1,66 @@
-import React, {useState} from 'react'
-import './navbar.css'
-import {MdModeOfTravel} from 'react-icons/md';
+import React, { useState } from 'react';
+import './navbar.css';
+import { MdModeOfTravel } from 'react-icons/md';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { TbGridDots } from 'react-icons/tb';
 
-
 const Navbar = () => {
-  const [active, setActive] =useState('navBar')
-  const showNav=()=>{
-    setActive('navBar activeNavbar')
-  }
-  const removeNav=()=>{
-    setActive('navbar')
-  }
+  const [active, setActive] = useState('navBar');
+
+  const showNav = () => {
+    setActive('navBar activeNavbar');
+  };
+
+  const removeNav = () => {
+    setActive('navBar');
+  };
+
   return (
-   <section className='navBarsection'>
-    <div className="header">
-      <div className="logoDiv">
-        <a href="#" className="logo">
-          <h1 className="Flex"><MdModeOfTravel className="icon"/> ExploreBosnia
-          </h1>
-        </a>
+    <section className="navBarSection">
+      <div className="header">
+        <div className="logoDiv">
+          <a href="#" className="logo">
+            <h1 className="flex">
+              <MdModeOfTravel className="icon" /> ExploreBosnia
+            </h1>
+          </a>
+        </div>
+        <div className={active}>
+          <ul className="navLists flex">
+            <li className="navItem">
+              <a href="#" className="navLink">Home</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">About</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">Contact</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">Blog</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">Resources</a>
+            </li>
+          </ul>
+          <div className="headerBtns flex">
+            <button className="btn loginBtn">
+              <a href="#">Login</a>
+            </button>
+            <button className="btn">
+              <a href="#">Sign in</a>
+            </button>
+          </div>
+          <div onClick={removeNav} className="closeNavbar">
+            <AiFillCloseCircle className="icon" />
+          </div>
+        </div>
+        <div onClick={showNav} className="toggleNavbar">
+          <TbGridDots className="icon" />
+        </div>
       </div>
-<div className={active}>
-  <ul className="navlists flex">
-<li className="navItem">
-  <a href="#" className="navLink">Home</a>
+    </section>
+  );
+};
 
-</li>
-<li className="navItem">
-  <a href="#" className="navLink">About</a>
-  
-</li>
-<li className="navItem">
-  <a href="#" className="navLink">Contact</a>
-  
-</li>
-<li className="navItem">
-  <a href="#" className="navLink">Blog</a>
-  
-</li>
-<li className="navItem">
-  <a href="#" className="navLink">Resources</a>
-  
-</li>
-<div className="hedaerBtns flex">
-  <button className="btn loginBtn">
-    <a href="#">Login</a>
-  </button>
-  <button className="btn">
-    <a href="#">Sign in</a>
-  </button>
-</div>
-  </ul>
-<div onClick={removeNav} 
-className="closeNavbar">
-< AiFillCloseCircle className="icon"/>
-</div>
-</div>
-<div onClick={showNav} className="toggleNavbar">
-<TbGridDots className="icon"/>
-</div>
-    </div>
-
-   </section>
-  )
-}
-
-export default Navbar
+export default Navbar;
