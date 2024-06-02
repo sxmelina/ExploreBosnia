644b4278385d1a98af14359a442c9e87f54bee73
Melina Salijević <148011168+sxmelina@users.noreply.github.com>
2024-06-02 13:19:21 +0200

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, {UseEffect} from 'react'
 import './about.css'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 const About = () => {
-  return (
+  useEffect(()=>{
+  Aos.init({duration:2000})
+  },[])
+  
+return (
     <div>About</div>
   )
 }
 
 export default About
+
+/* aos
+treba se dodati  u sva 3  diva singleItem : data-aos ="fade-up " data-aos-duration="2000" 
+ali u drugom duration 2500 a u trecem 300
+
+div cardText : data-aos ="fade-right" data-aos-duration="2000"
+div cardVideo :  data-aos ="fade-left " data-aos-duration="2000" */
